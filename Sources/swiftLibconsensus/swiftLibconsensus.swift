36129ce0ff10104f151208f3961a5469f37eee13
fanquake <fanquake@gmail.com>
2019-04-07 21:50:00 +0800

    add version

--- Sources/swiftLibconsensus/swiftLibconsensus.swift
+++ Sources/swiftLibconsensus/swiftLibconsensus.swift
@@ -1,3 +1,8 @@
-struct swiftLibconsensus {
-    var text = "Hello, World!"
+import libbitcoinconsensus
+
+public struct swiftLibconsensus {
+
+    public static var version: UInt32 {
+        return bitcoinconsensus_version()
+    }
 }
